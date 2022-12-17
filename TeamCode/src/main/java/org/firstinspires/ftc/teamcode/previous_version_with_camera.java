d143b408ca4d7557c0417f8265d9eddbdfd8ed25
Sviatoslav <loomyapsvr@gmail.com>
2022-12-16 20:11:12 -0600

    multithreading + mini autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/previous_version_with_camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/previous_version_with_camera.java
@@ -1,196 +1,196 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
-@Autonomous(name="Auto", group="Pushbot")
-public class auto extends LinearOpMode {
-
-    public robotInit robot = new robotInit();
-    ElapsedTime runtime = new ElapsedTime();
-
-    OpenCvCamera webcam;
-    SkystoneDeterminationPipeline pipeline;
-
-    @Override
-    public void runOpMode() {
-
-        robot.init(hardwareMap);
-
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
-
-        pipeline = new SkystoneDeterminationPipeline();
-        webcam.setPipeline(pipeline);
-
-  
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-            }
-        });
-
-
-        // Wait for the game to start (driver presses PLAY)
-        telemetry.addLine("Waiting for start");
-        telemetry.update();
-        waitForStart();
-
-        //Place wobble goal in the correct target zone
-        if (pipeline.position == SkystoneDeterminationPipeline.RingPosition.FOUR) {
-
-        }
-
-
-        else if (pipeline.position == SkystoneDeterminationPipeline.RingPosition.ONE) {
-
-        }
-
-
-        else {
-
-
-
-
-        }
-
-        // Stop, take a well deserved breather
-        sleep(1000);     // pause for servos to move
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-
-    }
-
-
-    /* FUNCTIONS */
-
-
-
-    /* ENCODER FUNCTIONS */
-
-
-
-
-    /* VUFORIA RING DETECTION */
-    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
-    {
-        /*
-         * An enum to define the skystone position
-         */
-        public enum RingPosition
-        {
-            FOUR,
-            ONE,
-            NONE
-        }
-
-        /*
-         * Some color constants
-         */
-        static final Scalar BLUE = new Scalar(0, 0, 255);
-        static final Scalar GREEN = new Scalar(0, 255, 0);
-
-        /*
-         * The core values which define the location and size of the sample regions
-         */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(85,183);
-
-        static final int REGION_WIDTH = 50;
-        static final int REGION_HEIGHT = 50;
-
-        final int FOUR_RING_THRESHOLD = 135; //143-148
-        final int ONE_RING_THRESHOLD = 125; //119-131
-
-        Point region1_pointA = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x,
-                REGION1_TOPLEFT_ANCHOR_POINT.y);
-        Point region1_pointB = new Point(
-                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
-        /*
-         * Working variables
-         */
-        Mat region1_Cb;
-        Mat YCrCb = new Mat();
-        Mat Cb = new Mat();
-        int avg1;
-
-        // Volatile since accessed by OpMode thread w/o synchronization
-        public volatile RingPosition position = RingPosition.FOUR;
-
-        /*
-         * This function takes the RGB frame, converts to YCrCb,
-         * and extracts the Cb channel to the 'Cb' variable
-         */
-        void inputToCb(Mat input)
-        {
-            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-            Core.extractChannel(YCrCb, Cb, 1);
-        }
-
-        @Override
-        public void init(Mat firstFrame)
-        {
-            inputToCb(firstFrame);
-
-            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
-        }
-
-        @Override
-        public Mat processFrame(Mat input)
-        {
-            inputToCb(input);
-
-            avg1 = (int) Core.mean(region1_Cb).val[0];
-
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
-
-            position = RingPosition.FOUR; // Record our analysis
-            if(avg1 > FOUR_RING_THRESHOLD){
-                position = RingPosition.FOUR;
-            }else if (avg1 > ONE_RING_THRESHOLD){
-                position = RingPosition.ONE;
-            }else{
-                position = RingPosition.NONE;
-            }
-
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region1_pointA, // First point which defines the rectangle
-                    region1_pointB, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    1); // Negative thickness means solid fill
-
-            return input;
-        }
-
-        public int getAnalysis()
-        {
-            return avg1;
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//import org.opencv.core.Core;
+//import org.opencv.core.Mat;
+//import org.opencv.core.Point;
+//import org.opencv.core.Rect;
+//import org.opencv.core.Scalar;
+//import org.opencv.imgproc.Imgproc;
+//import org.openftc.easyopencv.OpenCvCamera;
+//import org.openftc.easyopencv.OpenCvCameraFactory;
+//import org.openftc.easyopencv.OpenCvCameraRotation;
+//import org.openftc.easyopencv.OpenCvPipeline;
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+//
+//@Autonomous(name="Auto", group="Pushbot")
+//public class auto extends LinearOpMode {
+//
+//    public robotInit robot = new robotInit();
+//    ElapsedTime runtime = new ElapsedTime();
+//
+//    OpenCvCamera webcam;
+//    SkystoneDeterminationPipeline pipeline;
+//
+//    @Override
+//    public void runOpMode() {
+//
+//        robot.init(hardwareMap);
+//
+//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
+//
+//        pipeline = new SkystoneDeterminationPipeline();
+//        webcam.setPipeline(pipeline);
+//
+//
+//        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+//        {
+//            @Override
+//            public void onOpened()
+//            {
+//                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+//            }
+//        });
+//
+//
+//        // Wait for the game to start (driver presses PLAY)
+//        telemetry.addLine("Waiting for start");
+//        telemetry.update();
+//        waitForStart();
+//
+//        //Place wobble goal in the correct target zone
+//        if (pipeline.position == SkystoneDeterminationPipeline.RingPosition.FOUR) {
+//
+//        }
+//
+//
+//        else if (pipeline.position == SkystoneDeterminationPipeline.RingPosition.ONE) {
+//
+//        }
+//
+//
+//        else {
+//
+//
+//
+//
+//        }
+//
+//        // Stop, take a well deserved breather
+//        sleep(1000);     // pause for servos to move
+//
+//        telemetry.addData("Path", "Complete");
+//        telemetry.update();
+//
+//    }
+//
+//
+//    /* FUNCTIONS */
+//
+//
+//
+//    /* ENCODER FUNCTIONS */
+//
+//
+//
+//
+//    /* VUFORIA RING DETECTION */
+//    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
+//    {
+//        /*
+//         * An enum to define the skystone position
+//         */
+//        public enum RingPosition
+//        {
+//            FOUR,
+//            ONE,
+//            NONE
+//        }
+//
+//        /*
+//         * Some color constants
+//         */
+//        static final Scalar BLUE = new Scalar(0, 0, 255);
+//        static final Scalar GREEN = new Scalar(0, 255, 0);
+//
+//        /*
+//         * The core values which define the location and size of the sample regions
+//         */
+//        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(85,183);
+//
+//        static final int REGION_WIDTH = 50;
+//        static final int REGION_HEIGHT = 50;
+//
+//        final int FOUR_RING_THRESHOLD = 135; //143-148
+//        final int ONE_RING_THRESHOLD = 125; //119-131
+//
+//        Point region1_pointA = new Point(
+//                REGION1_TOPLEFT_ANCHOR_POINT.x,
+//                REGION1_TOPLEFT_ANCHOR_POINT.y);
+//        Point region1_pointB = new Point(
+//                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+//                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+//
+//        /*
+//         * Working variables
+//         */
+//        Mat region1_Cb;
+//        Mat YCrCb = new Mat();
+//        Mat Cb = new Mat();
+//        int avg1;
+//
+//        // Volatile since accessed by OpMode thread w/o synchronization
+//        public volatile RingPosition position = RingPosition.FOUR;
+//
+//        /*
+//         * This function takes the RGB frame, converts to YCrCb,
+//         * and extracts the Cb channel to the 'Cb' variable
+//         */
+//        void inputToCb(Mat input)
+//        {
+//            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+//            Core.extractChannel(YCrCb, Cb, 1);
+//        }
+//
+//        @Override
+//        public void init(Mat firstFrame)
+//        {
+//            inputToCb(firstFrame);
+//
+//            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
+//        }
+//
+//        @Override
+//        public Mat processFrame(Mat input)
+//        {
+//            inputToCb(input);
+//
+//            avg1 = (int) Core.mean(region1_Cb).val[0];
+//
+//            Imgproc.rectangle(
+//                    input, // Buffer to draw on
+//                    region1_pointA, // First point which defines the rectangle
+//                    region1_pointB, // Second point which defines the rectangle
+//                    BLUE, // The color the rectangle is drawn in
+//                    2); // Thickness of the rectangle lines
+//
+//            position = RingPosition.FOUR; // Record our analysis
+//            if(avg1 > FOUR_RING_THRESHOLD){
+//                position = RingPosition.FOUR;
+//            }else if (avg1 > ONE_RING_THRESHOLD){
+//                position = RingPosition.ONE;
+//            }else{
+//                position = RingPosition.NONE;
+//            }
+//
+//            Imgproc.rectangle(
+//                    input, // Buffer to draw on
+//                    region1_pointA, // First point which defines the rectangle
+//                    region1_pointB, // Second point which defines the rectangle
+//                    GREEN, // The color the rectangle is drawn in
+//                    1); // Negative thickness means solid fill
+//
+//            return input;
+//        }
+//
+//        public int getAnalysis()
+//        {
+//            return avg1;
+//        }
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotInit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotInit.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
@@ -15,13 +14,18 @@ public class robotInit {
     public DcMotor motorFR;
     public DcMotor motorBL;
     public DcMotor motorBR;
-    public DcMotor armLift;
-    public DcMotor spinner;
+    public DcMotor armLiftLeft;
+    public DcMotor armLiftRight;
+    public DcMotor waiter;
 //    public DcMotor armLift; //arm lifting mechanism
 
 
-//    public CRServo spinner; // spins the motor to bring in the cone
-    public Servo closer; // keeps the cone in place
+//  Servos intilization (Claw left and right part)
+//    Keep cone in place
+
+    public Servo closerL;
+    public Servo closerR;
+
 
     //from Encoder Sample
     double     COUNTS_PER_MOTOR_REV    = 537.7 ;
@@ -51,16 +55,20 @@ public class robotInit {
         motorFR = hardwareMap.get(DcMotor.class, "motor_fr");
         motorBL = hardwareMap.get(DcMotor.class, "motor_bl");
         motorBR = hardwareMap.get(DcMotor.class, "motor_br");
-        armLift = hardwareMap.get(DcMotor.class, "armLift");
-        spinner = hardwareMap.get(DcMotor.class, "spinner");
+        armLiftLeft = hardwareMap.get(DcMotor.class, "armLiftLeft");
+        armLiftRight = hardwareMap.get(DcMotor.class, "armLiftRight");
+        waiter = hardwareMap.get(DcMotor.class, "waiter");
+
+//        spinner = hardwareMap.get(DcMotor.class, "spinner");
 
         // Set the direction of the DC motors
         motorFL.setDirection(DcMotor.Direction.REVERSE);
         motorFR.setDirection(DcMotor.Direction.FORWARD);
         motorBL.setDirection(DcMotor.Direction.FORWARD);
         motorBR.setDirection(DcMotor.Direction.REVERSE);
-        armLift.setDirection(DcMotor.Direction.FORWARD); //Not sure which direction
-        spinner.setDirection(DcMotor.Direction.FORWARD); //Not sure which direction
+        armLiftLeft.setDirection(DcMotor.Direction.FORWARD); //Not sure which direction
+        armLiftRight.setDirection(DcMotor.Direction.REVERSE); //Not sure which direction
+        waiter.setDirection(DcMotor.Direction.FORWARD); //Not sure which direction
 
 
         // Set all DC motors to zero power
@@ -68,8 +76,9 @@ public class robotInit {
         motorBR.setPower(0);
         motorFR.setPower(0);
         motorFL.setPower(0);
-        armLift.setPower(0);
-        spinner.setPower(0);
+        armLiftLeft.setPower(0);
+        armLiftRight.setPower(0);
+        waiter.setPower(0);
 
 
         // Set all motors to run without encoders.
@@ -78,16 +87,17 @@ public class robotInit {
         motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        spinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Not sure
-        armLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Not sure
+        waiter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Not sure
+        armLiftLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Not sure
+        armLiftRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Not sure
 
 
         // Define and initialize ALL installed servos.
-        closer = hardwareMap.get(Servo.class, "closer");
+        closerL = hardwareMap.get(Servo.class, "closerL");
+        closerR = hardwareMap.get(Servo.class, "closerR");
 
-        //init servos
-        //freightSnatcher1.setPosition(0.72);
-        //freightSnatcher2.setPosition(0.4);
+
+
 
     }
 }
